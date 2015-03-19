5396f7cd97735107eac0aec9985831f8f17fabf8
su-kaiyao <1250471161@qq.com>
2015-03-19 14:10:05 +0800

    Heap && Graph

--- DataStructure/BinaryTree.java
+++ DataStructure/BinaryTree.java
@@ -1,5 +1,3 @@
-import java.util.Stack;
-
 /**
  * Binary Tree Demonstration
  * 
---
+++ DataStructure/Graph.java
@@ -0,0 +1,169 @@
+/**
+ * Node Demonstration
+ * 
+ * @author sukai
+ *
+ */
+class Vertex {
+    public char    label;
+    public boolean wasVisited;
+
+    public Vertex(char lab) {
+        label = lab;
+        wasVisited = false;
+    }
+}
+
+/**
+ * Stack Demonstration
+ * 
+ * @author sukai
+ *
+ */
+class StackX {
+    private final int SIZE = 20;
+    private int[]     st;
+    private int       top;
+
+    public StackX() {
+        st = new int[SIZE];
+        top = -1;
+    }
+
+    public void push(int j) {
+        st[++top] = j;
+    }
+
+    public int pop() {
+        return st[top--];
+    }
+
+    public int peek() {
+        return st[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+}
+
+/**
+ * Queue Demonstration
+ */
+class Queue {
+    private final int SIZE = 20;
+    private int[]     queArray;
+    private int       front;
+    private int       rear;
+
+    public Queue() {
+        queArray = new int[SIZE];
+        front = 0;
+        rear = -1;
+    }
+
+    public void intsert(int j) {
+        if (rear == SIZE - 1) {
+            rear = -1;
+        }
+        queArray[++rear] = j;
+    }
+
+    public int remove() {
+        int temp = queArray[front++];
+        if (front == SIZE) {
+            front = 0;
+        }
+        return temp;
+    }
+
+    public boolean isEmpty() {
+        return (rear + 1 == front || (front + SIZE - 1 == rear));
+    }
+}
+
+public class Graph {
+    private final int MAX_VERTS = 30;
+    private Vertex    vertexList[];
+    private int       adjMat[][];
+    private int       nVerts;
+    private StackX    theStack;
+    private Queue     theQueue;
+
+    public Graph() {
+        vertexList = new Vertex[MAX_VERTS];
+        adjMat = new int[MAX_VERTS][MAX_VERTS];
+        nVerts = 0;
+        for (int j = 0; j < MAX_VERTS; j++) {
+            for (int k = 0; k < MAX_VERTS; k++) {
+                adjMat[j][k] = 0;
+            }
+        }
+        theStack = new StackX();
+    }
+
+    public void addVertex(char lab) {
+        vertexList[nVerts++] = new Vertex(lab);
+    }
+
+    public void addEdge(int start, int end) {
+        adjMat[start][end] = 1;
+        adjMat[end][start] = 1;
+    }
+
+    public void displayVertex(int v) {
+        System.out.print(vertexList[v].label);
+    }
+
+    /*
+     * dfs
+     */
+    public void dfs() {
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        theStack.push(0);
+        while (!theStack.isEmpty()) {
+            int v = getAdjUnvisitedVertex(theStack.peek());
+            if (v == -1) {
+                theStack.pop();
+            } else {
+                vertexList[v].wasVisited = true;
+                displayVertex(v);
+                theStack.push(v);
+            }
+        }
+        for (int j = 0; j < nVerts; j++) {
+            vertexList[j].wasVisited = false;
+        }
+    }
+
+    /*
+     * bfs
+     */
+    public void bfs() {
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        theQueue.intsert(0);
+        int v2;
+        while (!theQueue.isEmpty()) {
+            int v1 = theQueue.remove();
+            while ((v2=getAdjUnvisitedVertex(v1)) != -1) {
+                vertexList[v2].wasVisited = true;
+                displayVertex(v2);
+                theQueue.intsert(v2);;
+            }
+        }
+        for (int j = 0; j < nVerts; j++) {
+            vertexList[j].wasVisited = false;
+        }
+    }
+
+    public int getAdjUnvisitedVertex(int v) {
+        for (int j = 0; j < nVerts; j++) {
+            if (adjMat[v][j] == 1 && vertexList[j].wasVisited == false) {
+                return j;
+            }
+        }
+        return -1;
+    }
+}
