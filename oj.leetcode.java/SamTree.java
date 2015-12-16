0dc33302c3f7605a8c9ebac8d349cae369cdb356
su-kaiyao <1250471161@qq.com>
2015-12-16 10:51:26 +0800

    leetcode 100 same tree

---
+++ oj.leetcode.java/SamTree.java
@@ -0,0 +1,29 @@
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            if (p.val != q.val) {
+                return false;
+            } else {
+                return (isSameTree(p.left, q.left)) && (isSameTree(p.right, q.right));
+            }
+        } else if (p == null && q == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
