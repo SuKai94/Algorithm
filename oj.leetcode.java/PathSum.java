3b6d1e91b9b5d78bbc8aa99bf7876a8c5924e7ea
su-kaiyao <1250471161@qq.com>
2015-12-19 14:16:32 +0800

    leetcode 112 Path Sum

---
+++ oj.leetcode.java/PathSum.java
@@ -0,0 +1,26 @@
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int flag=1;
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root==null) {
+            return false;
+        }
+        if (root.left==null && root.right==null) {
+            return root.val==sum;
+        }
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
