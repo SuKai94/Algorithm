e50f32a474df0c384af36b7eecdf87cd5bd2aead
su-kaiyao <1250471161@qq.com>
2015-12-19 14:20:30 +0800

    leetcode 292 Nim Game

---
+++ oj.leetcode.java/NimGame.java
@@ -0,0 +1,15 @@
+
+public class Solution {
+
+    public boolean canWinNim(int n) {
+        if (n%4==0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
